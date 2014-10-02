c54896f7bb5eb9ff93bf02b964408d48191b3690
Francisco Demartino <demartino.francisco@gmail.com>
2014-10-02 00:00:50 -0300

    Add metadata block for greasemonkey

---
+++ greentext.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        greentext
+// @namespace   greentext
+// @description >greentext.js
+// @include     *
+// @version     1
+// @grant       none
+// @downloadURL https://raw.githubusercontent.com/franciscod/greentext/master/greentext.min.js
+// @updateURL https://raw.githubusercontent.com/franciscod/greentext/master/greentext.meta.js
+// ==/UserScript==
