ea336f28dafdce57a55f35b1c2e9f0d8033b3dd0
Francisco Demartino <demartino.francisco@gmail.com>
2014-10-01 23:23:04 -0300

    v2 (?) add min.js

--- greentext.js
+++ greentext.js
@@ -1,3 +1,19 @@
-var gt = function (s) {if (!s) return; var fc = s.firstChild; if (!fc) return; var text = fc.nodeValue; if (!text) return; if (text.substr(0,1) != '>') return; s.style.color = "#789922";}
+var gt = function (s) {
+  if (!s) return;
+  var fc = s.firstChild;
+  if (!fc) return;
+  var text = fc.nodeValue;
+  if (!text) return;
+  if (text.substr(0,1) != '>') return;
+  s.style.color = "#789922";
+};
 
-for (k in document.getElementsByTagName('span')) { gt(ss[k]); } for (k in document.getElementsByTagName('div')) { gt(ss[k]); }
+var gtt = function (t) {
+  var es = document.getElementsByTagName(t);
+  for (i in es) {
+    gt(es[i]);
+  }
+};
+
+gtt('div');
+gtt('span');
