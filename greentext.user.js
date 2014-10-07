1d8cfaef59dba99cb274a9f7d42200dc8c9e0759
Francisco Demartino <demartino.francisco@gmail.com>
2014-10-07 16:14:07 -0300

    >le occam's razor

--- greentext.user.js
+++ greentext.user.js
@@ -3,10 +3,10 @@
 // @namespace   greentext
 // @description >greentext.js
 // @include     *
-// @version     8
+// @version     9
 // @grant       none
 // @require     https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.4/zepto.min.js
 // @downloadURL https://raw.githubusercontent.com/franciscod/greentext/master/greentext.user.js
 // @updateURL   https://raw.githubusercontent.com/franciscod/greentext/master/greentext.meta.js
 // ==/UserScript==
-var gt=function(s){if(!s)return;var fc=s.firstChild;if(!fc)return;var text=fc.nodeValue;if(!text)return;if(text.substr(0,1)!=">")return;s.style.color="#789922"};var gte=function(){var es=$("*").not("input").not(".editable");for(var i=es.length;i--;){gt(es[i])}};setInterval(gte,200);
+var gt=function(s){if(!s)return;var fc=s.firstChild;if(!fc)return;var text=fc.nodeValue;if(!text)return;if(text.substr(0,1)!=">")s.style.color="";else s.style.color="#789922"};var gte=function(){$("*").each(function(i){gt(this)})};setInterval(gte,200);
--- src/greentext.js
+++ src/greentext.js
@@ -1,18 +1,19 @@
 var gt = function (s) {
   if (!s) return;
-  var fc = s.firstChild;
-  if (!fc) return;
-  var text = fc.nodeValue;
-  if (!text) return;
-  if (text.substr(0,1) != '>') return;
-  s.style.color = "#789922";
+
+  var fc = s.firstChild; if (!fc) return;
+  var text = fc.nodeValue; if (!text) return;
+
+  if (text.substr(0,1) != '>')
+    s.style.color = "";
+  else
+    s.style.color = "#789922";
 };
 
 var gte = function() {
-  var es = $("*").not("input").not(".editable");
-  for (var i = es.length; i--;) {
-      gt(es[i]);
-  }
+  $("*").each(function(i) {
+      gt(this);
+  });
 };
 
 setInterval(gte, 200);
