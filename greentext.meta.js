5d7a54b1b8d22922f5183a71e58a0f139db45549
Francisco Demartino <demartino.francisco@gmail.com>
2014-10-07 16:01:05 -0300

    >zepto now coming from cdnjs

--- greentext.meta.js
+++ greentext.meta.js
@@ -3,8 +3,9 @@
 // @namespace   greentext
 // @description >greentext.js
 // @include     *
-// @version     5
+// @version     6
 // @grant       none
+// @require     https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.4/zepto.min.js
 // @downloadURL https://raw.githubusercontent.com/franciscod/greentext/master/greentext.user.js
 // @updateURL   https://raw.githubusercontent.com/franciscod/greentext/master/greentext.meta.js
 // ==/UserScript==
