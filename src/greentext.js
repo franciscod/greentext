864a90ae56af87b74579337a9470ead00d8f235a
Francisco Demartino <demartino.francisco@gmail.com>
2015-03-04 22:03:56 -0300

    >simpler

--- src/greentext.js
+++ src/greentext.js
@@ -6,16 +6,8 @@ function isTextNode() { return this.nodeType === 3; };
 function greentextChild() { return Zepto(this).parent().is('mark.greentexted'); };
 function isGreentext() { return Zepto(this).text().trim().substr(0,1) === '>'; };
 
-// light
-function greentextAll() {
-  Zepto('*').contents().filter(isTextNode).not(greentextChild).filter(isGreentext).map(applyGreentext);
-  Zepto('mark.greentexted').not(isGreentext).unwrap();
-};
-
-// heavy
 function greentextTextNodes() {
   Zepto('*').contents().filter(isTextNode).not(greentextChild).filter(isGreentext).map(applyGreentext);
 }
 
 greentextTextNodes();
-setInterval(greentextAll, 200);
